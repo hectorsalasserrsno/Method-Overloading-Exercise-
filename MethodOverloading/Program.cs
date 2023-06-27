b497dfda66c425eef8ed92ca7324bd2797f1b512
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-26 22:05:47 -0500

    complete excercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,46 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            
+            Add( 8, 9);
+            Add( 2.3m, 5.7m);
+            Add(0, 1, true);
+            
         }
+        public static void Add(int num1, int num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        public static void Add(decimal num1, decimal num2)
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        
+        public static void Add(int num1, int num2, bool isMoney)
+            {
+             
+                int total= num1 + num2;
+                if (isMoney == true && total > 1) 
+
+                {
+                Console.WriteLine($"{total} dollars");
+                }
+                else if (total == 1 && isMoney == true)
+                    
+                {
+                    Console.WriteLine($"{total} dollar");
+                }
+                else
+                {
+                    Console.WriteLine($"{total}");
+                }
+                
+
+
+            }
+        
     }
 }
